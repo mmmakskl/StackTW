7593274991354f28d27f006c52c7a3676248d1dc
Kladkovoi Maksim <cladkovoj@yandex.ru>
2024-03-21 20:49:28 +0300

    maybe the final commit

---
+++ abstract.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+
+template<typename T>
+class Stack
+{
+public:
+    virtual ~Stack() {}
+    virtual void push(const T& e) = 0;
+    virtual T pop() = 0;
+    virtual bool isEmpty() = 0;
+};
